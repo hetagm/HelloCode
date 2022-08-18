9b499127b31f49d4b874eaf32ecf166b7c4541e0
Khetag <hetagm77@gmail.com>
2022-08-18 11:18:55 +0300

    add seminar1

---
+++ SEMINARS_001/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.Write("Input number: ");
+int number = Convert. ToInt32(Console.ReadLine());
+
+int kvadrat = number * number;
+Console.WriteLine("The quad of " + number + " is " + kvadrat);
+
+
+Console.WriteLine($"The quad of {number} is {kvadrat}");
