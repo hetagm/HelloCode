2d46504dce48dd71e52986b5616997e9fe615982
Khetag <hetagm77@gmail.com>
2022-08-21 20:04:43 +0300

    add task2

--- SEMINAR_001_Z1/Program.cs
+++ SEMINAR_001_Z1/Program.cs
@@ -19,16 +19,16 @@
 
 //Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
 
-// Console.Write("Введите число: ");
-// int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
 
-// int current = num * (-1);
+int current = num * (-1);
 
-// while (current <= num)
-// {
-//     Console.Write(current);
-//     current = current +1;
-// }
+while (current <= num)
+{
+    Console.Write(current + " ");
+    current = current +1;
+}
 
 // Задача. Консольный ввод параметров
 
@@ -49,21 +49,21 @@
 
 // Задача. Сравнение двух чисел
 
-Console.Write("Введите первое число: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите второе число: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
 
-if(num1 > num2)
-{
-    Console.WriteLine($"Число {num1} больше числа {num2}");
-}
-else if (num1 < num2)
-{
-    Console.WriteLine($"Число {num1} меньше числа {num2}");
-}
-else
-{
-    Console.WriteLine("Число num1 равно числу num2");
-}
+// if(num1 > num2)
+// {
+//     Console.WriteLine($"Число {num1} больше числа {num2}");
+// }
+// else if (num1 < num2)
+// {
+//     Console.WriteLine($"Число {num1} меньше числа {num2}");
+// }
+// else
+// {
+//     Console.WriteLine("Число num1 равно числу num2");
+// }
