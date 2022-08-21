b206df0e46f19b163224eccdde230b576edeb8ea
Khetag <hetagm77@gmail.com>
2022-08-21 17:54:51 +0300

    correction

--- SEMINARS_001/Program.cs
+++ SEMINARS_001/Program.cs
@@ -3,7 +3,6 @@ Console.Write("Input number: ");
 int number = Convert. ToInt32(Console.ReadLine());
 
 int kvadrat = number * number;
-Console.WriteLine("The quad of " + number + " is " + kvadrat);
-
+// Console.WriteLine("The quad of " + number + " is " + kvadrat);
 
 Console.WriteLine($"The quad of {number} is {kvadrat}");
